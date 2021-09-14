f17b6d6cf6e14d4173e61c313f02a984d0f8f883
sss3287 <sss3287@rit.edu>
2021-09-14 13:47:50 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+
+def test_drive():
+    turtle.forward(100)
+    turtle.left(87)
+    turtle.setheading(127)
+    turtle.up()
+    turtle.goto(50, 50)
+    turtle.down()
+    turtle.home()
+    turtle.circle(50)
+    input()
+
+def main():
+    test_drive()
+
+main()
