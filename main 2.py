f73184e91908da50f42f9f6e536c874eee3bebcb
sss3287 <sss3287@rit.edu>
2021-09-15 11:08:52 +0400

    initial commit

---
+++ main 2.py
@@ -0,0 +1,21 @@
+def days_of_week():
+    print ("Sunday","Monday","Tuesday")
+    print ("Wednesday","Thursday","Friday")
+    print ("Saturday")
+
+def birthdate():
+    print ("November 3rd,2003")
+
+def box():
+    print ("*****")
+    print ("*   *")
+    print ("******")
+
+pi = "3.14159"
+year = input("Enter the current year:")
+box()
+days_of_week()
+birthdate()
+box()
+
+
