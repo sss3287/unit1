b25005dbbf0e1481d84511fd9f9b088ab08072ea
sss3287 <sss3287@rit.edu>
2021-09-26 13:53:02 +0400

    Initial Commit

---
+++ Assignment/main.py
@@ -0,0 +1,14 @@
+import turtle
+
+def rectangle_will_fit(x,y,length,height):
+    if x > -200 and x < 200 :
+        print ("Yes")
+    elif  y > -200 and y < 200:
+        print ("Yes")
+    else:
+        print ("No")
+
+def main():
+    rectangle_will_fit(100,150,50,60)
+
+    main()
